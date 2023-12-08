d771193d4c904f85825e2eda171a7a23d55eb81e
PolarbearHQ <146632357+PolarbearHQ@users.noreply.github.com>
2023-12-08 21:45:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <link href='https://fonts.googleapis.com/css?family=Gugi' rel='stylesheet'>
-  <link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet'>
-</head>
-<body style="margin:0px;padding:0px;font-family:Orbitron;">
-  <header style="background-color:teal;padding-bottom:10px">
-  	<div style="display:flex">
-      <img src="AA_1080.png" style="width:150px;height:150px;marin:10px;verticle-align:middle" alt="AA Logo"></img>
-      <p style="color:white;font-family:Gugi;font-size:50px;margin-left:20px;verticle-align:middle">The Axiom Arcade</p>
+  <head>
+    <link href='https://fonts.googleapis.com/css?family=Gugi' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet'>
+    <style>
+      nav {
+        background-color:white;
+        padding:3px;
+        display:flex;
+      }
+      nav a {
+        color:white;
+        text-decoration:none;
+        background-color:teal;
+        padding:10px;
+        margin-left:3px;
+      }
+    </style>
+  </head>
+  <body style="margin:0px;padding:0px;font-family:Orbitron;">
+    <header style="background-color:teal;padding-bottom:10px">
+      <div style="display:flex">
+        <img src="AA_1080.png" style="width:150px;height:150px;marin:10px;verticle-align:middle" alt="AA Logo"></img>
+        <p style="color:white;font-family:Gugi;font-size:50px;margin-left:20px;verticle-align:middle">The Axiom Arcade</p>
+      </div>
+      <nav>
+        <a href="index.php" style="margin:0px">HOME</a>
+        <a href="minecraft.php">MINECRAFT</a>
+        <div style="background-color:teal;width:100%;margin-left:3px"></div>
+        <a href="about.php">ABOUT</a>
+      </nav>
+    </header>
+    <div style="margin:20px">
+    <h1>Hi there!</h1>
+    <p>This site is currently a W.I.P.</p>
     </div>
-    <nav style="background-color:white;padding:3px;display:flex">
-      <a href="index.php" style="color:white;text-decoration:none;background-color:teal;padding:10px;margin-right:3px">HOME</a>
-      <a href="minecraft.php" style="color:white;text-decoration:none;background-color:teal;padding:10px;margin-right:3px">MINECRAFT</a>
-      <div style="background-color:teal;width:100%"></div>
-      <a href="about.php" style="color:white;text-decoration:none;background-color:teal;padding:10px;margin-left:3px">ABOUT</a>
-    </nav>
-  </header>
-  <div style="margin:20px">
-  <h1>Hi there!</h1>
-  <p>This site is currently a W.I.P.</p>
-  </div>
-</body>
+  </body>
 </html>
